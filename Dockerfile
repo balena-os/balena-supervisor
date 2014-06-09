272daea89ca1bd9fb459f1e23172437074cf98e5
Petros Angelatos <petrosagg@gmail.com>
2014-06-09 23:48:42 +0100

    Make supervisor deployable with resin

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,3 @@
 FROM resin/rpi-buildstep:v0.0.7
 ADD . /app
-ADD deploy_key /root/.ssh/id_rsa
-RUN chmod 400 /root/.ssh/id_rsa
-ADD known_hosts /root/.ssh/known_hosts
 RUN /build/builder
-ENTRYPOINT ["/app/entry.sh"]
--- deps.sh
+++ deps.sh
@@ -1,2 +1,9 @@
+# Deploy key for private npm modules
+mkdir -p /root/.ssh
+cp deploy_key /root/.ssh/id_rsa
+chmod 400 /root/.ssh/id_rsa
+cp known_hosts /root/.ssh/known_hosts
+
+# System dependencies
 apt-get update
 apt-get install -y openvpn libsqlite3-dev python
