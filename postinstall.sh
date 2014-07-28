3bad30eff7eba741a81e76a7e7070cdb96b60331
Pagan Gazzard <pjgazzard@googlemail.com>
2014-07-28 15:10:24 +0100

    Remove the coffeescript source files and coffeescript module after compilation since they're only needed at compile time (and not at runtime).

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,3 @@
 node ./node_modules/coffee-script/bin/coffee -c ./src 
-rm -rf /root/.ssh/* /build/app/deploy_key /app/deploy_key /build/app/Makefile /app/Makefile
+rm -rf /root/.ssh/* /build/app/deploy_key /app/deploy_key /build/app/Makefile /app/Makefile /app/src/*.coffee
+npm uninstall coffee-script
