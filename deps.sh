0495053fd559cb9a4417a67f745a33945e4a3beb
Pagan Gazzard <pjgazzard@googlemail.com>
2014-08-01 16:20:58 +0100

    Add production mode guards.

--- deps.sh
+++ deps.sh
@@ -1,8 +1,10 @@
-# Deploy key for private npm modules
-mkdir -p /root/.ssh
-cp deploy_key /root/.ssh/id_rsa
-chmod 400 /root/.ssh/id_rsa
-cp ssh_config /root/.ssh/config
+if [ $NODE_ENV == 'production' ]; then
+	# Deploy key for private npm modules
+	mkdir -p /root/.ssh
+	cp deploy_key /root/.ssh/id_rsa
+	chmod 400 /root/.ssh/id_rsa
+	cp ssh_config /root/.ssh/config
+fi
 
 # System dependencies
 apt-get -q update
