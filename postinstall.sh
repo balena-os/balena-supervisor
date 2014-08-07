370f1bf7ce6cddb195ea3f51fa3389dce2a1d848
Pagan Gazzard <pjgazzard@googlemail.com>
2014-08-07 21:21:53 +0100

    Make sure the npm and apt-get caches are cleaned.

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,9 @@ if [ $NODE_ENV == 'production' ]; then
 	node ./node_modules/coffee-script/bin/coffee -c ./src
 	# We don't need coffee-script at runtime
 	npm uninstall coffee-script
+	# Empty the apt and npm caches of the packages we installed
+	npm cache clean
+	apt-get clean
 	# Remove deploy key
 	rm -rf /root/.ssh/* deploy_key
 	# Remove unnecessary source files
