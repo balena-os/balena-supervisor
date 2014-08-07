550aebb1637c45be03ac9b3982afcf5ff20b8604
Pagan Gazzard <pjgazzard@googlemail.com>
2014-08-07 22:23:01 +0100

    Remove tests and docs, we don't need them at runtime.

--- postinstall.sh
+++ postinstall.sh
@@ -15,4 +15,8 @@ if [ $NODE_ENV == 'production' ]; then
 	find . -name '*.c' -delete
 	# And the tar files (sqlite3 module again)
 	find . -name '*.tar.*' -delete
+	# Who needs tests and docs? Pffft! - Ignoring errors because find isn't a fan of us deleting directories whilst it's trying to search within them.
+	find . -type d -name 'test' -exec rm -rf '{}' \; 2> /dev/null || true
+	find . -type d -name 'doc' -exec rm -rf '{}' \; 2> /dev/null || true
+	find . -type d -name 'man' -exec rm -rf '{}' \; 2> /dev/null || true
 fi
