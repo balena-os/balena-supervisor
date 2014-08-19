33441c74e727ec39a6c6451ae72fc6fa05185414
Pagan Gazzard <pjgazzard@googlemail.com>
2014-08-19 17:46:38 +0100

    Silence the setting up of the socket.
    This was appearing whenever you started a new tty.js terminal - leaking stuff that shouldn't really be.

--- src/enterContainer.sh
+++ src/enterContainer.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 SOCKET_NAME=test-${1}-${2}-${3}
 HOST_DATA_PATH=/resin-data/resin-supervisor
 HOST_SOCKET=${HOST_DATA_PATH}/${SOCKET_NAME}
@@ -7,5 +8,5 @@ echo "
 	rm -f ${HOST_SOCKET}
 	socat UNIX-LISTEN:${HOST_SOCKET} EXEC:'${HOST_DATA_PATH}/enter.sh ${1}',pty,setsid,setpgid,stderr,ctty &
 	exit
-" | socat UNIX:${SUPERVISOR_SOCKET_PATH}/host -
+" | socat UNIX:${SUPERVISOR_SOCKET_PATH}/host - >& /dev/null
 socat UNIX:${SUPERVISOR_SOCKET_PATH}/${SOCKET_NAME} -,raw,echo=0
