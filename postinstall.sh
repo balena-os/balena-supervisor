8c774549ad5174a6b92247860c9be894aaeb3c57
Pagan Gazzard <pjgazzard@googlemail.com>
2014-10-01 14:21:17 +0100

    A hack to use our custom css for tty.js - CSS comes from Eugene.

--- postinstall.sh
+++ postinstall.sh
@@ -3,6 +3,7 @@ set -o pipefail
 
 if [ $NODE_ENV == 'production' ]; then
 	chmod +x src/enterContainer.sh
+	cp ttyjs-static/* node_modules/tty.js/static/
 
 	node ./node_modules/coffee-script/bin/coffee -c ./src
 	# We don't need coffee-script at runtime
