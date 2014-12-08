b1b039cdd18de9e3bd4208e8ac4874aeb0fc6a72
Pagan Gazzard <pjgazzard@googlemail.com>
2014-12-08 18:00:01 +0000

    Check if the container is running, and if not then print a meaningful error message.

--- bin/enter.sh
+++ bin/enter.sh
@@ -1,1 +1,7 @@
-nsenter --target $(docker inspect --format '{{.State.Pid}}' $1) --mount --uts --ipc --net --pid bash
+#!/bin/bash
+CONTAINER_PID=$(docker inspect --format '{{if .State.Running}}{{.State.Pid}}{{end}}' $1)
+if [ -z $CONTAINER_PID ]; then
+	read -p "Application must be running for a terminal to be started."
+else
+	nsenter --target $CONTAINER_PID --mount --uts --ipc --net --pid bash
+fi
