75b696d3d6c8aa96732a8db92ac54b44eb03c65c
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-09-10 18:53:57 +0000

    Populate supervisor config using tags in a SupervisorConfig struct.

--- gosuper/gosuper/config.go
+++ gosuper/gosuper/config.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"reflect"
+	"strconv"
 )
 
 type UserConfig struct {
@@ -43,26 +44,39 @@ type SupervisorConfig struct {
 	VpnStatusPath         string `config_env:"VPN_STATUS_PATH",config_default:"/mnt/root/run/openvpn/vpn_status"`
 }
 
+func populateConfigStruct(value reflect.Value) {
+	valueType = value.Type()
+	for i := 0; i < value.NumField(); i++ {
+		f := value.Field(i)
+		envValue := os.Getenv(valueType.Field(i).Tag.Get("config_env"))
+		defaultValue := valueType.Field(i).Tag.Get("config_default")
+		switch f.Kind() {
+		case reflect.String:
+			if envValue != "" {
+				f.SetString(envValue)
+			} else if defaultValue != "" {
+				f.SetString(defaultValue)
+			}
+		case reflect.Int:
+			if envValue != "" {
+				f.SetInt(strconv.Atoi(envValue))
+			} else if defaultValue != "" {
+				f.SetInt(strconv.Atoi(defaultValue))
+			}
+		case reflect.Struct:
+			populate(f, f.Type)
+		default:
+			log.Printf("Unknown config type %s in field %s\n", f.Type(), valueType.Field(i).Name)
+		}
+	}
+}
+
+func populateConfig(v interface{}) {
+	value = reflect.ValueOf(v).Elem()
+	populateConfigStruct(value)
+}
+
 func GetSupervisorConfig() (config SupervisorConfig) {
-	config.ApiEndpoint = os.Getenv("GOSUPER_SOCKET")
-	/*
-		apiEndpoint: process.env.API_ENDPOINT ? 'https://api.resin.io'
-		listenPort: process.env.LISTEN_PORT ? 80
-		gosuperAddress: "http://unix:#{process.env.GOSUPER_SOCKET}:"
-		registryEndpoint: process.env.REGISTRY_ENDPOINT ? 'registry.resin.io'
-		pubnub:
-			subscribe_key: process.env.PUBNUB_SUBSCRIBE_KEY ? 'sub-c-bananas'
-			publish_key: process.env.PUBNUB_PUBLISH_KEY ? 'pub-c-bananas'
-		mixpanelToken: process.env.MIXPANEL_TOKEN ? 'bananasbananas'
-		dockerSocket: process.env.DOCKER_SOCKET ? '/run/docker.sock'
-		supervisorImage: process.env.SUPERVISOR_IMAGE ? 'resin/rpi-supervisor'
-		configMountPoint: process.env.CONFIG_MOUNT_POINT ? '/mnt/mmcblk0p1/config.json'
-		ledFile: process.env.LED_FILE ? '/sys/class/leds/led0/brightness'
-		bootstrapRetryDelay: checkInt(process.env.BOOTSTRAP_RETRY_DELAY_MS) ? 30000
-		restartSuccessTimeout: checkInt(process.env.RESTART_SUCCESS_TIMEOUT) ? 60000
-		appUpdatePollInterval: checkInt(process.env.APPLICATION_UPDATE_POLL_INTERVAL) ? 60000
-		successMessage: 'SUPERVISOR OK'
-		forceApiSecret: process.env.RESIN_SUPERVISOR_SECRET ? null
-		vpnStatusPath: process.env.VPN_STATUS_PATH ? '/mnt/root/run/openvpn/vpn_status'
-	*/
+	populateConfig(&config)
+	return
 }
