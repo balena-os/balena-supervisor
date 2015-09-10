27786ebe65c593312a8e47944f8231a5255a8709
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-09-10 16:11:05 +0000

    Quick fix: use quotes in bash comparison

--- retry_docker_push.sh
+++ retry_docker_push.sh
@@ -3,11 +3,11 @@
 IMAGE=$1
 retries=0
 
-while [ $retries -lt 3 ]; do
+while [ "$retries" -lt 3 ]; do
 	let retries=retries+1
 	docker push $IMAGE
 	ret=$?
-	if [ "$ret" -eq "0" ]; then
+	if [ "$ret" -eq 0 ]; then
 		break
 	fi
 done
