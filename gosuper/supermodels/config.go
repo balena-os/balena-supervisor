98fa4d0ab838f6e581d05dab0e4db0ac99e120c9
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-09-11 14:53:06 +0000

    Add BoltDB, and restructure gosuper with an initial attempt at passing dependencies between packages. Ensured unit tests pass.

---
+++ gosuper/supermodels/config.go
@@ -0,0 +1,12 @@
+package supermodels
+
+type Config struct {
+}
+
+func (config Config) Get(key string) (val string, err error) {
+	return
+}
+
+func (config Config) Set(key string) (val string, err error) {
+	return
+}
