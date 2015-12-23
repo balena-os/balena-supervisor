34dcae78debb895e8df52e30c5ff4bf397ad5438
Praneeth Bodduluri <lifeeth@gmail.com>
2015-12-23 10:54:09 +0530

    Fix formatting tests to exclude Godeps

--- gosuper/test_formatting.sh
+++ gosuper/test_formatting.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-if [[ -n "$(gofmt -l .)" ]]; then
-	echo "Bad formatting, run make format-gosuper to fix it."
-	exit 1
-else
-	echo "Formatting test passed."
-fi
+DIRS=`ls -l . | egrep '^d' | awk '{print $9}'`
+
+DIRS=`echo $DIRS | sed "s/Godeps//g"`
+
+# and finally loop over the cleaned up directory list.
+for DIR in $DIRS
+do
+	if [[ -n "$(gofmt -l ${DIR})" ]]; then
+		echo "Bad formatting, run make format-gosuper to fix it."
+		exit 1
+	fi
+done
+echo "Formatting test passed."
