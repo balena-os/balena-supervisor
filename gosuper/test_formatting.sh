7490e784c46c7cb7778ca4ecbba9beda49291056
Praneeth Bodduluri <lifeeth@gmail.com>
2016-01-11 21:23:19 +0530

    Fix test_formatting to output the errors as well

--- gosuper/test_formatting.sh
+++ gosuper/test_formatting.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 for dir in $(find ./* -path ./Godeps -prune -or -type d -print); do
-	if [[ -n "$(gofmt -l ${dir})" ]]; then
-		echo "Bad formatting, run make format-gosuper to fix it."
-		exit 1
+	errormessage=$(gofmt -l $dir)
+	if [ $errormessage ]
+	then
+		echo "$errormessage"
+		failed=true
 	fi
 done
+
+if [ $failed ]
+then
+	echo "Bad formatting, run make format-gosuper to fix above errors."
+	exit 1
+fi
