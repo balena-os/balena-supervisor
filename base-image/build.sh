08b64592e22c65d438c74c6226eb87db58d6a9ab
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-06-30 22:54:17 +0700

    add automation script for supervisor-base images

---
+++ base-image/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+
+BUILD_DIR='/home/builder/tmp'
+
+mkdir -p $BUILD_DIR
+cp -r $SOURCE_DIR/* $BUILD_DIR/
+cd $BUILD_DIR
+source oe-core/oe-init-build-env build bitbake
+bitbake core-image-minimal
+qemu=$(cat conf/local.conf | grep '^MACHINE ??= ' | grep -o '"[^"]\+"' | tr -d '"')
+cp --dereference tmp/deploy/images/$qemu/core-image-minimal-$qemu.tar.bz2  $DEST_DIR/rootfs.tar.bz2
