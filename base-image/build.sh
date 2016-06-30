6554034202055d90a6306aad3af92535def253b8
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-06-30 22:54:17 +0700

    automation script

---
+++ base-image/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+
+BUILD_DIR='/home/builder/tmp'
+
+mkdir -p $BUILD_DIR
+cp -r $SOURCE_DIR/* $BUILD_DIR/
+cd $BUILD_DIR
+source poky/oe-init-build-env build
+bitbake core-image-minimal
+cd tmp/deploy/images/
+cp $(find . -name *.rootfs.tar.bz2) $DEST_DIR/rootfs.tar.bz2
