e1dd444b415eb698064d9c7774513655ed8f4343
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-06-30 22:54:17 +0700

    add automation script for supervisor-base images

---
+++ base-image/automation/jenkins-build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+date=$(date +'%Y%m%d' -u)
+REPO='resin/amd64-supervisor-base'
+
+git submodule update --init --recursive
+rm -rf dest
+mkdir dest
+
+docker build -t supervisor-base-builder .
+docker run --rm \
+	-v `pwd`:/source \
+	-v `pwd`/dest:/dest \
+	supervisor-base-builder
+if [ -f dest/rootfs.tar.bz2 ]; then
+	cat dest/rootfs.tar.bz2 | docker import - $REPO:$date
+	docker tag -f $REPO:$date $REPO:latest
+	docker push $REPO
+else
+	echo "rootfs is missing!"
+	exit 1
+fi
