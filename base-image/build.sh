f93b72afa833f66125f37e5817aa5a4fd870551a
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-07-05 22:13:55 +0700

    Add build info for multi-arch (i386, armel, armv6, armv7hf)

--- base-image/build.sh
+++ base-image/build.sh
@@ -8,6 +8,5 @@ mkdir -p $BUILD_DIR
 cp -r $SOURCE_DIR/* $BUILD_DIR/
 cd $BUILD_DIR
 source oe-core/oe-init-build-env build bitbake
-bitbake core-image-minimal
-qemu=$(cat conf/local.conf | grep '^MACHINE ??= ' | grep -o '"[^"]\+"' | tr -d '"')
-cp --dereference tmp/deploy/images/$qemu/core-image-minimal-$qemu.tar.gz  $DEST_DIR/rootfs.tar.gz
+MACHINE=$TARGET_MACHINE bitbake core-image-minimal
+cp --dereference tmp-glibc/deploy/images/$TARGET_MACHINE/core-image-minimal-$TARGET_MACHINE.tar.gz  $DEST_DIR/rootfs.tar.gz
