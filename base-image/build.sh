42c06d4e3f1e481d016618004bfafe2c6b6beba0
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-07-05 22:13:55 +0700

    Add build info for multi-arch (i386, armel, armv6, armv7hf)

--- base-image/build.sh
+++ base-image/build.sh
@@ -8,6 +8,6 @@ mkdir -p $BUILD_DIR
 cp -r $SOURCE_DIR/* $BUILD_DIR/
 cd $BUILD_DIR
 source poky/oe-init-build-env build
+sed -e s~#{MACHINE_NAME}~"$TARGET_MACHINE"~g conf/local.conf.tpl > conf/local.conf
 bitbake core-image-minimal
-qemu=$(cat conf/local.conf | grep '^MACHINE ??= ' | grep -o '"[^"]\+"' | tr -d '"')
-cp --dereference tmp/deploy/images/$qemu/core-image-minimal-$qemu.tar.gz  $DEST_DIR/rootfs.tar.gz
+cp --dereference tmp/deploy/images/$TARGET_MACHINE/core-image-minimal-$TARGET_MACHINE.tar.gz  $DEST_DIR/rootfs.tar.gz
