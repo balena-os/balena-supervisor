375a286a0fa037bdd84de7545228aa65c6c0d8dd
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-07-05 22:13:55 +0700

    Add build info for multi-arch (i386, armel, armv6, armv7hf)

--- base-image/automation/jenkins-build.sh
+++ base-image/automation/jenkins-build.sh
@@ -5,21 +5,43 @@ set -o pipefail
 
 date=$(date +'%Y%m%d' -u)
 REPO='resin/amd64-supervisor-base'
+# MACHINE_LIST: generic-x86-64 generic-x86 generic-armv6 generic-armv7hf generic-armv5
+# MACHINE_LIST should be set in jenkins config
 
 git submodule update --init --recursive
 rm -rf dest
 mkdir dest
 
 docker build -t supervisor-base-builder .
-docker run --rm \
-	-v `pwd`:/source \
-	-v `pwd`/dest:/dest \
-	supervisor-base-builder
-if [ -f dest/rootfs.tar.gz ]; then
-	docker import dest/rootfs.tar.gz $REPO:$date
-	docker tag -f $REPO:$date $REPO:latest
-	docker push $REPO
-else
-	echo "rootfs is missing!"
-	exit 1
-fi
+for machine in $MACHINE_LIST; do
+	case "$device" in
+	'generic-x86-64')
+		REPO='resin/amd64-supervisor-base'
+	;;
+	'generic-x86')
+		REPO='resin/i386-supervisor-base'
+	;;
+	'generic-armv6')
+		REPO='resin/armv6-supervisor-base'
+	;;
+	'generic-armv7hf')
+		REPO='resin/armv7hf-supervisor-base'
+	;;
+	'generic-armv5')
+		REPO='resin/armel-supervisor-base'
+	;;
+	esac
+	docker run --rm \
+		-e TARGET_MACHINE=$machine \
+		-v `pwd`:/source \
+		-v `pwd`/dest:/dest \
+		supervisor-base-builder
+	if [ -f dest/rootfs.tar.gz ]; then
+		docker import dest/rootfs.tar.gz $REPO:$date
+		docker tag -f $REPO:$date $REPO:latest
+		docker push $REPO
+	else
+		echo "rootfs is missing!"
+		exit 1
+	fi
+done
--- base-image/build.sh
+++ base-image/build.sh
@@ -8,6 +8,5 @@ mkdir -p $BUILD_DIR
 cp -r $SOURCE_DIR/* $BUILD_DIR/
 cd $BUILD_DIR
 source poky/oe-init-build-env build
-bitbake core-image-minimal
-qemu=$(cat conf/local.conf | grep '^MACHINE ??= ' | grep -o '"[^"]\+"' | tr -d '"')
-cp --dereference tmp/deploy/images/$qemu/core-image-minimal-$qemu.tar.gz  $DEST_DIR/rootfs.tar.gz
+MACHINE=$TARGET_MACHINE bitbake core-image-minimal
+cp --dereference tmp/deploy/images/$TARGET_MACHINE/core-image-minimal-$TARGET_MACHINE.tar.gz  $DEST_DIR/rootfs.tar.gz
