2fb71f73450b0781d0797e4fcc782b95564c23fe
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-07-08 11:04:23 +0700

    use shared folders (downloads and sstate) in yocto build

--- base-image/build.sh
+++ base-image/build.sh
@@ -3,10 +3,11 @@
 set -o errexit
 
 BUILD_DIR='/home/builder/tmp'
-
-mkdir -p $BUILD_DIR
-cp -r $SOURCE_DIR/* $BUILD_DIR/
-cd $BUILD_DIR
-source oe-core/oe-init-build-env build bitbake
-MACHINE=$TARGET_MACHINE bitbake core-image-minimal
-cp --dereference tmp-glibc/deploy/images/$TARGET_MACHINE/core-image-minimal-$TARGET_MACHINE.tar.gz  $DEST_DIR/rootfs.tar.gz
+groupadd -g $BUILDER_GID builder
+useradd -m -u $BUILDER_UID -g $BUILDER_GID builder
+sudo -H -u builder /bin/bash -c "mkdir -p $BUILD_DIR \
+	&& cp -r $SOURCE_DIR/* $BUILD_DIR/ \
+	&& cd $BUILD_DIR \
+	&& source oe-core/oe-init-build-env build bitbake \
+	&& DL_DIR=$SHARED_DOWNLOADS SSTATE_DIR=$SHARED_SSTATE MACHINE=$TARGET_MACHINE $BUILD_DIR/bitbake/bin/bitbake core-image-minimal"
+cp --dereference $BUILD_DIR/build/tmp-glibc/deploy/images/$TARGET_MACHINE/core-image-minimal-$TARGET_MACHINE.tar.gz  $DEST_DIR/rootfs.tar.gz
