5e5a28130730fe1340b1a50cf7a8b580f67b085e
Pablo Carranza Velez <pablo@resin.io>
2016-10-05 19:49:53 +0000

    Remove unused Jenkins build step

--- automation/jenkins_build.sh
+++ automation/jenkins_build.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Jenkins build steps
-VERSION=$(git describe --always --abbrev=6)
 ESCAPED_BRANCH_NAME=$(echo $sourceBranch | sed 's/[^a-z0-9A-Z_.-]/-/g')
 
 # Try pulling the old build first for caching purposes.
@@ -12,15 +11,15 @@ docker pull registry.resinstaging.io/resin/node-supervisor-${ARCH}:${ESCAPED_BRA
 docker pull registry.resinstaging.io/resin/go-supervisor-${ARCH}:${ESCAPED_BRANCH_NAME} || docker pull registry.resinstaging.io/resin/go-supervisor-${ARCH}:master || true
 
 # Test the gosuper
-make SUPERVISOR_VERSION=${VERSION} JOB_NAME=${JOB_NAME} test-gosuper
+make SUPERVISOR_VERSION=${ESCAPED_BRANCH_NAME} JOB_NAME=${JOB_NAME} test-gosuper
 
 MAKE_ARGS="ARCH=${ARCH} \
     PUBNUB_SUBSCRIBE_KEY=${PUBNUB_SUBSCRIBE_KEY} \
     PUBNUB_PUBLISH_KEY=${PUBNUB_PUBLISH_KEY} \
-    MIXPANEL_TOKEN=${MIXPANEL_TOKEN}"
+    MIXPANEL_TOKEN=${MIXPANEL_TOKEN} \
+    SUPERVISOR_VERSION=${ESCAPED_BRANCH_NAME}"
 
 make ${MAKE_ARGS} \
-    SUPERVISOR_VERSION=${ESCAPED_BRANCH_NAME} \
     DEPLOY_REGISTRY= \
     deploy
 
@@ -31,16 +30,6 @@ docker push registry.resinstaging.io/resin/node-supervisor-${ARCH}:${ESCAPED_BRA
 docker push registry.resinstaging.io/resin/go-supervisor-${ARCH}:${ESCAPED_BRANCH_NAME} || true
 
 make ${MAKE_ARGS} \
-    SUPERVISOR_VERSION=${VERSION} \
-    DEPLOY_REGISTRY= \
-    deploy
-
-make ${MAKE_ARGS} \
-    SUPERVISOR_VERSION=${ESCAPED_BRANCH_NAME} \
     DEPLOY_REGISTRY=registry.resinstaging.io/ \
     deploy
 
-make ${MAKE_ARGS} \
-    SUPERVISOR_VERSION=${VERSION} \
-    DEPLOY_REGISTRY=registry.resinstaging.io/ \
-    deploy
