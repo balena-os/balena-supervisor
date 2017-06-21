d837cba2cd766640ade56e1b49c1d6178e63f927
Pablo Carranza Velez <pablo@resin.io>
2017-06-20 21:11:45 -0700

    Use a variable for alternative db path in tests
    Signed-off-by: Pablo Carranza Velez <pablo@resin.io>

--- test/test.js
+++ test/test.js
@@ -4,12 +4,12 @@ var fs = require('fs')
 process.env.ROOT_MOUNTPOINT = './test/data'
 process.env.CONFIG_JSON_PATH = '/config.json'
 process.env.DATABASE_PATH = './test/data/database.sqlite'
-
+process.env.DATABASE_PATH_2 = './test/data/database2.sqlite'
 try {
 	fs.unlinkSync(process.env.DATABASE_PATH)
 } catch(err){}
 try {
-	fs.unlinkSync('./dbfoo.sqlite')
+	fs.unlinkSync(process.env.DATABASE_PATH_2)
 } catch(err){}
 try {
 	fs.unlinkSync(process.env.ROOT_MOUNTPOINT + process.env.CONFIG_JSON_PATH)
