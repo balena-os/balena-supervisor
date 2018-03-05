8eb3e6a8dd2f800cfb52cc12b1886e986ef84e5b
Petros Angelatos <petrosagg@gmail.com>
2018-03-04 17:30:48 -0800

    base-image: enable logs when building the yocto image
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- base-image/build.sh
+++ base-image/build.sh
@@ -44,7 +44,7 @@ sudo -H -u builder \
 	&& cp -r $SOURCE_DIR/* $BUILD_DIR/ \
 	&& cd $BUILD_DIR \
 	&& source oe-core/oe-init-build-env build bitbake \
-	&& DL_DIR=$SHARED_DOWNLOADS SSTATE_DIR=$SHARED_SSTATE MACHINE=$TARGET_MACHINE $BUILD_DIR/bitbake/bin/bitbake core-image-minimal > /dev/null"
+	&& DL_DIR=$SHARED_DOWNLOADS SSTATE_DIR=$SHARED_SSTATE MACHINE=$TARGET_MACHINE $BUILD_DIR/bitbake/bin/bitbake core-image-minimal"
 tar xzf $BUILD_DIR/build/tmp-glibc/deploy/images/$TARGET_MACHINE/core-image-minimal-$TARGET_MACHINE.tar.gz  -C $DEST_DIR
 # Delete the sstate and downloads directory so that the resulting image isn't huge
 # If https://github.com/moby/moby/issues/32507 gets implemented we can start using
