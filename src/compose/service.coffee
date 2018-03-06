enicky/resin-supervisor

compose: implement the models that make up multicontainer applications
This commit adds models to manage services, images, volumes and networks.

The main model for this is ServiceManager, which manages the collection of services on the device. It has functions to query what services are running, and to perform actions like starting, killing or performing handovers.

The Service model allows defining the transformations between a container and its service representation, and includes the functions to compare a running service with a target to determine if an update needs to happen.
This model includes the relevant compose file entries for a service that are supported. Bind mounts are disallowed except for the ones that relate to supervisor features, and persistent data is now stored in named volumes.

The Images model allows fetching and removing images, and includes functionality to determine images that have to be cleaned up - now only dangling and old supervisor images are cleaned up automatically, and ApplicationManager
will remove images that correspond to old services that are no longer needed.

The Networks and Volumes models allow managing named networks and volumes that are part of composed applications.

Changelog-Entry: Remove all bind mounts that were specific to 1.X devices. Move the resin-kill-me file for the handover strategy to /tmp/resin. Add environment variables for the location of resin-kill-me and the lockfile. Use running containers to determine what services are running instead of storing them in the internal database. Use named volumes for persistent data.
Change-Type: major
Signed-off-by: Pablo Carranza Velez <pablo@resin.io>

src/compose/service.coffee
@@ -1,0 +1,311 @@
+_ = require 'lodash'
+path = require 'path'
+{ checkTruthy, checkInt } = require '../lib/validation'
+updateLock = require '../lib/update-lock'
+constants = require '../lib/constants'
+conversions =  require '../lib/conversions'
+
+validRestartPolicies = [ 'no', 'always', 'on-failure', 'unless-stopped' ]
+
+# Construct a restart policy based on its name.
+# The default policy (if name is not a valid policy) is "always".
+createRestartPolicy = (name) ->
+	if not (name in validRestartPolicies)
+		name = 'unless-stopped'
+	return { Name: name, MaximumRetryCount: 0 }
+
+getCommand = (service, imageInfo) ->
+	if service.command?
+		return service.command
+	else if imageInfo?.Config?.Cmd
+		return imageInfo.Config.Cmd
+
+getEntrypoint = (service, imageInfo) ->
+	if service.entrypoint?
+		return service.entrypoint
+	else if imageInfo?.Config?.Entrypoint
+		return imageInfo.Config.Entrypoint
+
+killmePath = (appId, serviceName) ->
+	return updateLock.lockPath(appId, serviceName)
+
+defaultBinds = (appId, serviceName) ->
+	return [
+		"#{updateLock.lockPath(appId, serviceName)}:/tmp/resin"
+	]
+
+module.exports = class Service
+	constructor: (serviceProperties, opts = {}) ->
+		{
+			@image
+			@expose
+			@ports
+			@network_mode
+			@privileged
+			@releaseId
+			@imageId
+			@serviceId
+			@appId
+			@serviceName
+			@containerId
+			@running
+			@createdAt
+			@environment
+			@command
+			@entrypoint
+			@labels
+			@volumes
+			@restartPolicy
+			@depends_on
+			@cap_add
+			@cap_drop
+			@commit
+			@status
+		} = serviceProperties
+		@privileged ?= false
+		@volumes ?= []
+		@labels ?= {}
+		@environment ?= {}
+		@running ?= true
+		@ports ?= []
+		@expose ?= []
+		@cap_add ?= []
+		@cap_drop ?= []
+		@network_mode ?= @appId.toString()
+		if @releaseId?
+			@releaseId = @releaseId.toString()
+
+		# If the service has no containerId, it is a target service and has to be normalised and extended
+		if !@containerId?
+			@restartPolicy = createRestartPolicy(serviceProperties.restart)
+			@command = getCommand(serviceProperties, opts.imageInfo)
+			@entrypoint = getEntrypoint(serviceProperties, opts.imageInfo)
+			@extendEnvVars(opts)
+			@extendLabels(opts.imageInfo)
+			@extendAndSanitiseVolumes(opts.imageInfo)
+
+			if checkTruthy(@labels['io.resin.features.dbus'])
+				@volumes.push('/run/dbus:/host/run/dbus')
+			if checkTruthy(@labels['io.resin.features.kernel_modules'])
+				@volumes.push('/lib/modules:/lib/modules')
+			if checkTruthy(@labels['io.resin.features.firmware'])
+				@volumes.push('/lib/firmware:/lib/firmware')
+			if checkTruthy(@labels['io.resin.features.supervisor_api'])
+				@environment['RESIN_SUPERVISOR_HOST'] = opts.supervisorApiHost
+				@environment['RESIN_SUPERVISOR_PORT'] = opts.listenPort.toString()
+				@environment['RESIN_SUPERVISOR_ADDRESS'] = "http://#{opts.supervisorApiHost}:#{opts.listenPort}"
+				@environment['RESIN_SUPERVISOR_API_KEY'] = opts.apiSecret
+			if checkTruthy(@labels['io.resin.features.resin_api'])
+				@environment['RESIN_API_KEY'] = opts.deviceApiKey
+
+	extendEnvVars: ({ imageInfo, uuid, appName, commit, name, version, deviceType, osVersion }) =>
+		newEnv =
+			RESIN_APP_ID: @appId.toString()
+			RESIN_APP_NAME: appName
+			RESIN_APP_COMMIT: commit
+			RESIN_APP_RELEASE: @releaseId.toString()
+			RESIN_SERVICE_NAME: @serviceName
+			RESIN_DEVICE_UUID: uuid
+			RESIN_DEVICE_NAME_AT_INIT: name
+			RESIN_DEVICE_TYPE: deviceType
+			RESIN_HOST_OS_VERSION: osVersion
+			RESIN_SUPERVISOR_VERSION: version
+			RESIN_APP_LOCK_PATH: '/tmp/resin/resin-updates.lock'
+			RESIN_SERVICE_KILL_ME_PATH: '/tmp/resin/resin-kill-me'
+			RESIN: '1'
+			USER: 'root'
+		if @environment?
+			_.defaults(newEnv, @environment)
+		_.defaults(newEnv, conversions.envArrayToObject(imageInfo?.Config?.Env ? []))
+		@environment = newEnv
+		return @environment
+
+	extendLabels: (imageInfo) =>
+		@labels = _.clone(@labels)
+		_.defaults(@labels, imageInfo?.Config?.Labels ? {})
+		@labels['io.resin.supervised'] = 'true'
+		@labels['io.resin.app_id'] = @appId.toString()
+		@labels['io.resin.service_id'] = @serviceId.toString()
+		@labels['io.resin.service_name'] = @serviceName
+		@labels['io.resin.image_id'] = @imageId.toString()
+		@labels['io.resin.commit'] = @commit
+		return @labels
+
+	extendAndSanitiseVolumes: (imageInfo) =>
+		volumes = []
+		_.forEach @volumes, (vol) ->
+			isBind = /:/.test(vol)
+			if isBind
+				bindSource = vol.split(':')[0]
+				if !path.isAbsolute(bindSource)
+					volumes.push(vol)
+				else
+					console.log("Ignoring invalid bind mount #{vol}")
+			else
+				volumes.push(vol)
+		volumes = volumes.concat(@defaultBinds())
+		volumes = _.union(_.keys(imageInfo?.Config?.Volumes), volumes)
+		@volumes = volumes
+		return @volumes
+
+	getNamedVolumes: =>
+		defaults = @defaultBinds()
+		validVolumes = _.map @volumes, (vol) ->
+			return null if _.includes(defaults, vol)
+			return null if !/:/.test(vol)
+			bindSource = vol.split(':')[0]
+			if !path.isAbsolute(bindSource)
+				return bindSource
+			else
+				return null
+		return _.filter(validVolumes, (v) -> !_.isNull(v))
+
+	lockPath: =>
+		return updateLock.lockPath(@appId)
+
+	killmePath: =>
+		return killmePath(@appId, @serviceName)
+
+	killmeFullPathOnHost: =>
+		return "#{constants.rootMountPoint}#{@killmePath()}/resin-kill-me"
+
+	defaultBinds: ->
+		return defaultBinds(@appId, @serviceName)
+
+	@fromContainer: (container) ->
+		if container.State.Running
+			status = 'Running'
+		else if container.State.Status == 'created'
+			status = 'Installed'
+		else
+			status = 'Stopped'
+
+		boundContainerPorts = []
+		ports = []
+		expose = []
+		_.forEach container.HostConfig.PortBindings, (conf, port) ->
+			containerPort = port.match(/^([0-9]*)\/tcp$/)?[1]
+			if containerPort?
+				boundContainerPorts.push(containerPort)
+				hostPort = conf[0]?.HostPort
+				if !_.isEmpty(hostPort)
+					ports.push("#{containerPort}:#{hostPort}")
+				else
+					ports.push(containerPort)
+		_.forEach container.Config.ExposedPorts, (_, port) ->
+			containerPort = port.match(/^([0-9]*)\/tcp$/)?[1]
+			if containerPort? and !_.includes(boundContainerPorts, containerPort)
+				expose.push(containerPort)
+
+		appId = container.Config.Labels['io.resin.app_id']
+		serviceId = container.Config.Labels['io.resin.service_id']
+		serviceName = container.Config.Labels['io.resin.service_name']
+		releaseId = container.Name.match(/.*_(\d+)$/)?[1]
+		service = {
+			appId: appId
+			serviceId: serviceId
+			serviceName: serviceName
+			imageId: checkInt(container.Config.Labels['io.resin.image_id'])
+			command: container.Config.Cmd
+			entrypoint: container.Config.Entrypoint
+			network_mode: container.HostConfig.NetworkMode
+			volumes: _.concat(container.HostConfig.Binds ? [], _.keys(container.Config.Volumes ? {}))
+			image: container.Config.Image
+			environment: conversions.envArrayToObject(container.Config.Env)
+			privileged: container.HostConfig.Privileged
+			releaseId: releaseId
+			commit: container.Config.Labels['io.resin.commit']
+			labels: container.Config.Labels
+			running: container.State.Running
+			createdAt: new Date(container.Created)
+			restartPolicy: container.HostConfig.RestartPolicy
+			ports: ports
+			expose: expose
+			containerId: container.Id
+			cap_add: container.HostConfig.CapAdd
+			cap_drop: container.HostConfig.CapDrop
+			status
+		}
+		return new Service(service)
+
+	# TODO: map ports for any of the possible formats "container:host/protocol", port ranges, etc.
+	getPortsAndPortBindings: =>
+		ports = {}
+		portBindings = {}
+		if @ports?
+			_.forEach @ports, (port) ->
+				ports[port + '/tcp'] = {}
+				portBindings[port + '/tcp'] = [ HostPort: port ]
+		if @expose?
+			_.forEach @expose, (port) ->
+				ports[port + '/tcp'] = {}
+		return { ports, portBindings }
+
+	getBindsAndVolumes: =>
+		binds = []
+		volumes = {}
+		_.forEach @volumes, (vol) ->
+			isBind = /:/.test(vol)
+			if isBind
+				binds.push(vol)
+			else
+				volumes[vol] = {}
+		return { binds, volumes }
+
+	toContainerConfig: =>
+		{ ports, portBindings } = @getPortsAndPortBindings()
+		{ binds, volumes } = @getBindsAndVolumes()
+
+		conf = {
+			name: "#{@serviceName}_#{@releaseId}"
+			Image: @image
+			Cmd: @command
+			Entrypoint: @entrypoint
+			Tty: true
+			Volumes: volumes
+			Env: _.map @environment, (v, k) -> k + '=' + v
+			ExposedPorts: ports
+			Labels: @labels
+			HostConfig:
+				Privileged: @privileged
+				NetworkMode: @network_mode
+				PortBindings: portBindings
+				Binds: binds
+				RestartPolicy: @restartPolicy
+				CapAdd: @cap_add
+				CapDrop: @cap_drop
+		}
+		# If network mode is the default network for this app, add alias for serviceName
+		if @network_mode == @appId.toString()
+			conf.NetworkingConfig = {
+				EndpointsConfig: {
+					"#{@appId}": {
+						Aliases: [ @serviceName ]
+					}
+				}
+			}
+		return conf
+
+	isSameContainer: (otherService) =>
+		propertiesToCompare = [
+			'image'
+			'imageId'
+			'network_mode'
+			'privileged'
+			'restartPolicy'
+			'labels'
+			'environment'
+			'cap_add'
+			'cap_drop'
+		]
+		arraysToCompare = [
+			'ports'
+			'expose'
+			'volumes'
+		]
+		return _.isEqual(_.pick(this, propertiesToCompare), _.pick(otherService, propertiesToCompare)) and
+			_.every arraysToCompare, (property) =>
+				_.isEmpty(_.xorWith(this[property], otherService[property], _.isEqual))
+
+	isEqual: (otherService) =>
+		return @isSameContainer(otherService) and @running == otherService.running and @releaseId == otherService.releaseId

balena-os/balena-supervisor

src/compose/service.coffee
@@ -1,0 +1,312 @@
+_ = require 'lodash'
+path = require 'path'
+{ checkTruthy, checkInt } = require '../lib/validation'
+updateLock = require '../lib/update-lock'
+constants = require '../lib/constants'
+conversions =  require '../lib/conversions'
+
+validRestartPolicies = [ 'no', 'always', 'on-failure', 'unless-stopped' ]
+
+# Construct a restart policy based on its name.
+# The default policy (if name is not a valid policy) is "always".
+createRestartPolicy = (name) ->
+	if not (name in validRestartPolicies)
+		name = 'unless-stopped'
+	return { Name: name, MaximumRetryCount: 0 }
+
+getCommand = (service, imageInfo) ->
+	if service.command?
+		return service.command
+	else if imageInfo?.Config?.Cmd
+		return imageInfo.Config.Cmd
+
+getEntrypoint = (service, imageInfo) ->
+	if service.entrypoint?
+		return service.entrypoint
+	else if imageInfo?.Config?.Entrypoint
+		return imageInfo.Config.Entrypoint
+
+killmePath = (appId, serviceName) ->
+	return updateLock.lockPath(appId, serviceName)
+
+defaultBinds = (appId, serviceName) ->
+	return [
+		"#{updateLock.lockPath(appId, serviceName)}:/tmp/resin"
+	]
+
+module.exports = class Service
+	constructor: (serviceProperties, opts = {}) ->
+		{
+			@image
+			@expose
+			@ports
+			@network_mode
+			@privileged
+			@releaseId
+			@imageId
+			@serviceId
+			@appId
+			@serviceName
+			@containerId
+			@running
+			@createdAt
+			@environment
+			@command
+			@entrypoint
+			@labels
+			@volumes
+			@restartPolicy
+			@depends_on
+			@cap_add
+			@cap_drop
+			@commit
+			@status
+		} = serviceProperties
+		@privileged ?= false
+		@volumes ?= []
+		@labels ?= {}
+		@environment ?= {}
+		@running ?= true
+		@ports ?= []
+		@expose ?= []
+		@cap_add ?= []
+		@cap_drop ?= []
+		@network_mode ?= @appId.toString()
+		if @releaseId?
+			@releaseId = @releaseId.toString()
+
+		# If the service has no containerId, it is a target service and has to be normalised and extended
+		if !@containerId?
+			@restartPolicy = createRestartPolicy(serviceProperties.restart)
+			@command = getCommand(serviceProperties, opts.imageInfo)
+			@entrypoint = getEntrypoint(serviceProperties, opts.imageInfo)
+			@extendEnvVars(opts)
+			@extendLabels(opts.imageInfo)
+			@extendAndSanitiseVolumes(opts.imageInfo)
+
+			if checkTruthy(@labels['io.resin.features.dbus'])
+				@volumes.push('/run/dbus:/host/run/dbus')
+			if checkTruthy(@labels['io.resin.features.kernel_modules'])
+				@volumes.push('/lib/modules:/lib/modules')
+			if checkTruthy(@labels['io.resin.features.firmware'])
+				@volumes.push('/lib/firmware:/lib/firmware')
+			if checkTruthy(@labels['io.resin.features.supervisor_api'])
+				@environment['RESIN_SUPERVISOR_HOST'] = opts.supervisorApiHost
+				@environment['RESIN_SUPERVISOR_PORT'] = opts.listenPort.toString()
+				@environment['RESIN_SUPERVISOR_ADDRESS'] = "http://#{opts.supervisorApiHost}:#{opts.listenPort}"
+				@environment['RESIN_SUPERVISOR_API_KEY'] = opts.apiSecret
+			if checkTruthy(@labels['io.resin.features.resin_api'])
+				@environment['RESIN_API_KEY'] = opts.deviceApiKey
+
+	extendEnvVars: ({ imageInfo, uuid, appName, commit, name, version, deviceType, osVersion }) =>
+		newEnv =
+			RESIN_APP_ID: @appId.toString()
+			RESIN_APP_NAME: appName
+			RESIN_APP_COMMIT: commit
+			RESIN_APP_RELEASE: @releaseId.toString()
+			RESIN_SERVICE_NAME: @serviceName
+			RESIN_DEVICE_UUID: uuid
+			RESIN_DEVICE_NAME_AT_INIT: name
+			RESIN_DEVICE_TYPE: deviceType
+			RESIN_HOST_OS_VERSION: osVersion
+			RESIN_SUPERVISOR_VERSION: version
+			RESIN_APP_LOCK_PATH: '/tmp/resin/resin-updates.lock'
+			RESIN_SERVICE_KILL_ME_PATH: '/tmp/resin/resin-kill-me'
+			RESIN: '1'
+			USER: 'root'
+		if @environment?
+			_.defaults(newEnv, @environment)
+		_.defaults(newEnv, conversions.envArrayToObject(imageInfo?.Config?.Env ? []))
+		@environment = newEnv
+		return @environment
+
+	extendLabels: (imageInfo) =>
+		@labels = _.clone(@labels)
+		_.defaults(@labels, imageInfo?.Config?.Labels ? {})
+		@labels['io.resin.supervised'] = 'true'
+		@labels['io.resin.app_id'] = @appId.toString()
+		@labels['io.resin.service_id'] = @serviceId.toString()
+		@labels['io.resin.service_name'] = @serviceName
+		@labels['io.resin.image_id'] = @imageId.toString()
+		@labels['io.resin.commit'] = @commit
+		return @labels
+
+	extendAndSanitiseVolumes: (imageInfo) =>
+		volumes = []
+		_.forEach @volumes, (vol) ->
+			isBind = /:/.test(vol)
+			if isBind
+				bindSource = vol.split(':')[0]
+				if !path.isAbsolute(bindSource)
+					volumes.push(vol)
+				else
+					console.log("Ignoring invalid bind mount #{vol}")
+			else
+				volumes.push(vol)
+		volumes = volumes.concat(@defaultBinds())
+		volumes = _.union(_.keys(imageInfo?.Config?.Volumes), volumes)
+		@volumes = volumes
+		return @volumes
+
+	getNamedVolumes: =>
+		defaults = @defaultBinds()
+		validVolumes = _.map @volumes, (vol) ->
+			return null if _.includes(defaults, vol)
+			return null if !/:/.test(vol)
+			bindSource = vol.split(':')[0]
+			if !path.isAbsolute(bindSource)
+				return bindSource
+			else
+				return null
+		return _.filter(validVolumes, (v) -> !_.isNull(v))
+
+	lockPath: =>
+		return updateLock.lockPath(@appId)
+
+	killmePath: =>
+		return killmePath(@appId, @serviceName)
+
+	killmeFullPathOnHost: =>
+		return "#{constants.rootMountPoint}#{@killmePath()}/resin-kill-me"
+
+	defaultBinds: ->
+		return defaultBinds(@appId, @serviceName)
+
+	@fromContainer: (container) ->
+		if container.State.Running
+			status = 'Running'
+		else if container.State.Status == 'created'
+			status = 'Installed'
+		else
+			status = 'Stopped'
+
+		boundContainerPorts = []
+		ports = []
+		expose = []
+		_.forEach container.HostConfig.PortBindings, (conf, port) ->
+			containerPort = port.match(/^([0-9]*)\/tcp$/)?[1]
+			if containerPort?
+				boundContainerPorts.push(containerPort)
+				hostPort = conf[0]?.HostPort
+				if !_.isEmpty(hostPort)
+					ports.push("#{hostPort}:#{containerPort}")
+				else
+					ports.push(containerPort)
+		_.forEach container.Config.ExposedPorts, (conf, port) ->
+			containerPort = port.match(/^([0-9]*)\/tcp$/)?[1]
+			if containerPort? and !_.includes(boundContainerPorts, containerPort)
+				expose.push(containerPort)
+
+		appId = container.Config.Labels['io.resin.app_id']
+		serviceId = container.Config.Labels['io.resin.service_id']
+		serviceName = container.Config.Labels['io.resin.service_name']
+		releaseId = container.Name.match(/.*_(\d+)$/)?[1]
+		service = {
+			appId: appId
+			serviceId: serviceId
+			serviceName: serviceName
+			imageId: checkInt(container.Config.Labels['io.resin.image_id'])
+			command: container.Config.Cmd
+			entrypoint: container.Config.Entrypoint
+			network_mode: container.HostConfig.NetworkMode
+			volumes: _.concat(container.HostConfig.Binds ? [], _.keys(container.Config.Volumes ? {}))
+			image: container.Config.Image
+			environment: conversions.envArrayToObject(container.Config.Env)
+			privileged: container.HostConfig.Privileged
+			releaseId: releaseId
+			commit: container.Config.Labels['io.resin.commit']
+			labels: container.Config.Labels
+			running: container.State.Running
+			createdAt: new Date(container.Created)
+			restartPolicy: container.HostConfig.RestartPolicy
+			ports: ports
+			expose: expose
+			containerId: container.Id
+			cap_add: container.HostConfig.CapAdd
+			cap_drop: container.HostConfig.CapDrop
+			status
+		}
+		return new Service(service)
+
+	# TODO: map ports for any of the possible formats "container:host/protocol", port ranges, etc.
+	getPortsAndPortBindings: =>
+		ports = {}
+		portBindings = {}
+		if @ports?
+			_.forEach @ports, (port) ->
+				[ hostPort, containerPort ] = port.split(':')
+				ports[containerPort + '/tcp'] = {}
+				portBindings[containerPort + '/tcp'] = [ HostPort: hostPort ]
+		if @expose?
+			_.forEach @expose, (port) ->
+				ports[port + '/tcp'] = {}
+		return { ports, portBindings }
+
+	getBindsAndVolumes: =>
+		binds = []
+		volumes = {}
+		_.forEach @volumes, (vol) ->
+			isBind = /:/.test(vol)
+			if isBind
+				binds.push(vol)
+			else
+				volumes[vol] = {}
+		return { binds, volumes }
+
+	toContainerConfig: =>
+		{ ports, portBindings } = @getPortsAndPortBindings()
+		{ binds, volumes } = @getBindsAndVolumes()
+
+		conf = {
+			name: "#{@serviceName}_#{@releaseId}"
+			Image: @image
+			Cmd: @command
+			Entrypoint: @entrypoint
+			Tty: true
+			Volumes: volumes
+			Env: _.map @environment, (v, k) -> k + '=' + v
+			ExposedPorts: ports
+			Labels: @labels
+			HostConfig:
+				Privileged: @privileged
+				NetworkMode: @network_mode
+				PortBindings: portBindings
+				Binds: binds
+				RestartPolicy: @restartPolicy
+				CapAdd: @cap_add
+				CapDrop: @cap_drop
+		}
+		# If network mode is the default network for this app, add alias for serviceName
+		if @network_mode == @appId.toString()
+			conf.NetworkingConfig = {
+				EndpointsConfig: {
+					"#{@appId}": {
+						Aliases: [ @serviceName ]
+					}
+				}
+			}
+		return conf
+
+	isSameContainer: (otherService) =>
+		propertiesToCompare = [
+			'image'
+			'imageId'
+			'network_mode'
+			'privileged'
+			'restartPolicy'
+			'labels'
+			'environment'
+			'cap_add'
+			'cap_drop'
+		]
+		arraysToCompare = [
+			'ports'
+			'expose'
+			'volumes'
+		]
+		return _.isEqual(_.pick(this, propertiesToCompare), _.pick(otherService, propertiesToCompare)) and
+			_.every arraysToCompare, (property) =>
+				_.isEmpty(_.xorWith(this[property], otherService[property], _.isEqual))
+
+	isEqual: (otherService) =>
+		return @isSameContainer(otherService) and @running == otherService.running and @releaseId == otherService.releaseId