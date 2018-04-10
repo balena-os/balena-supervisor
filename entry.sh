baseboxorg/resin-supervisor

Add ability to use self-signed CAs passed via `config.json`.
Connects-to: #601
Change-type: minor

entry.sh
@@ -23,4 +23,13 @@ fi
 
 export DBUS_SYSTEM_BUS_ADDRESS="unix:path=/mnt/root/run/dbus/system_bus_socket"
 
+# Include self-signed CAs, should they exist
+if [ ! -z "${RESIN_ROOT_CA}" ]; then
+	if [ ! -e '/etc/ssl/certs/resinRootCA.pem' ]; then
+		mkdir -p /usr/local/share/ca-certificates
+		echo "${RESIN_ROOT_CA}" > /usr/local/share/ca-certificates/resinRootCA.crt
+		update-ca-certificates
+	fi
+fi
+
 exec node /usr/src/app/dist/app.js

balena-os/balena-supervisor

entry.sh
@@ -23,4 +23,13 @@ fi
 
 export DBUS_SYSTEM_BUS_ADDRESS="unix:path=/mnt/root/run/dbus/system_bus_socket"
 
+# Include self-signed CAs, should they exist
+if [ ! -z "${BALENA_ROOT_CA}" ]; then
+	if [ ! -e '/etc/ssl/certs/balenaRootCA.pem' ]; then
+		mkdir -p /usr/local/share/ca-certificates
+		echo "${BALENA_ROOT_CA}" > /usr/local/share/ca-certificates/balenaRootCA.crt
+		update-ca-certificates
+	fi
+fi
+
 exec node /usr/src/app/dist/app.js