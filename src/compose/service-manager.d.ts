311eaf0ac0f5ba433b3ec39c67088b4b2500290f
Cameron Diver <cameron@balena.io>
2018-11-27 22:05:39 +0000

    device-api: Add container id endpoint
    Change-type: minor
    Signed-off-by: Cameron Diver <cameron@balena.io>

--- src/compose/service-manager.d.ts
+++ src/compose/service-manager.d.ts
@@ -1,3 +1,4 @@
+import * as Bluebird from 'bluebird';
 import { EventEmitter } from 'events';
 
 import { Service } from '../compose/service';
@@ -5,6 +6,7 @@ import { Service } from '../compose/service';
 // FIXME: Unfinished definition for this class...
 declare class ServiceManager extends EventEmitter {
 	public getStatus(): Service[];
+	public getAll(): Bluebird<Service[]>;
 }
 
 export = ServiceManager;
