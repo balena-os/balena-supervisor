f9413ddd9b2a5c56ea4d3450cf67a9ddbf7b5978
Cameron Diver <cameron@balena.io>
2018-12-20 18:16:27 +0000

    TMP: Add livepush

--- sync.js
+++ sync.js
@@ -10,8 +10,8 @@ Usage:
 The script will first build a non-optimized version of the js code and sync the resulting app.js
 onto the supervisor container at the specified IP. It will also restart the supervisor container.
 The device must be a development variant of balenaOS and the supervisor must be running.
-	`)
-	process.exit(1)
+	`);
+	process.exit(1);
 }
 
 const childProcess = require('child_process');
@@ -19,12 +19,40 @@ const childProcess = require('child_process');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config');
 const compiler = webpack(webpackConfig({ noOptimize: true }));
+const Docker = require('dockerode');
+const { Container, FileUpdates } = require('livepush');
 
-const doSync = require('balena-sync').sync('local-balena-os-device').sync;
+// const doSync = require('balena-sync').sync('local-balena-os-device').sync;
+const doSync = async syncOpts => {
+	// Livepush doesn't support the type of Dockerfile that the supervisor uses
+	// (Multistage, variables etc...) so we generate a fake Dockerfile that will
+	// behave the same for our purposes
+	const dockerfile = `
+	FROM img
+	WORKDIR /usr/src/app
+	COPY ./dist ./
+	`;
+
+	const container = new Container(
+		dockerfile,
+		syncOpts.baseDir,
+		syncOpts.appName,
+		new Docker({ Host: syncOpts.deviceIp }),
+	);
+
+	const updates = new FileUpdates({
+		// TODO: Also read all migration files
+		updated: 'dist/app.js',
+		added: [],
+		deleted: [],
+	});
+	const actions = container.actionsNeeded(updates);
+	await container.performActions(updates, actions);
+};
 
 const syncOpts = {
 	deviceIp: process.argv[2],
-	baseDir: __dirname + '/dist',
+	baseDir: __dirname,
 	destination: '/usr/src/app/dist',
 	appName: 'resin_supervisor',
 	skipGitignore: true,
@@ -32,13 +60,16 @@ const syncOpts = {
 
 childProcess.execSync('npm install', { stdio: 'inherit' });
 
-compiler.watch({
-	ignored: /node_modules/,
-}, (err, stats) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log(stats.toString({ colors: true }));
-	doSync(syncOpts);
-});
+compiler.watch(
+	{
+		ignored: /node_modules/,
+	},
+	(err, stats) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log(stats.toString({ colors: true }));
+		doSync(syncOpts);
+	},
+);
