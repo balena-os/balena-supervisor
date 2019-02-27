dd2a506d7734030c640369ccedf3575a2a348269
Cameron Diver <cameron@balena.io>
2019-02-27 15:26:31 +0000

    Add migration for delta apply timeout without strict checking
    Change-type: patch
    Signed-off-by: Cameron Diver <cameron@balena.io>

---
+++ src/migrations/M00004.js
@@ -0,0 +1,21 @@
+exports.up = function(knex) {
+	return knex('deviceConfig')
+		.select('targetValues')
+		.then(([target]) => {
+			const targetValues = target.targetValues;
+			const jsonValues = JSON.parse(targetValues);
+
+			if (jsonValues['SUPERVISOR_DELTA_APPLY_TIMEOUT'] == '') {
+				jsonValues['SUPERVISOR_DELTA_APPLY_TIMEOUT'] = '0';
+
+				// Only update the database if we need to
+				return knex('deviceConfig').update({
+					targetValues: JSON.stringify(jsonValues),
+				});
+			}
+		});
+};
+
+exports.down = function(knex, Promise) {
+	return Promise.reject(new Error('Not Implemented'));
+};
