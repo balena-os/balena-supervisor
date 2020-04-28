2007a98940ea93f8064b045fce52133a4b48bac0
Miguel Casqueira <mig@xgho.st>
2020-04-27 22:21:03 -0400

    Added Bearer Authorization spec
    Closes: #1249
    Change-type: minor
    Signed-off-by: Miguel Casqueira <miguel@balena.io>

--- test/27-supervisor-api-auth.spec.ts
+++ test/27-supervisor-api-auth.spec.ts
@@ -1,13 +1,16 @@
 import { expect } from 'chai';
+import { fs } from 'mz';
 import * as requestLib from 'request';
+
 import Config from '../src/config';
 import Database from '../src/db';
 import EventTracker from '../src/event-tracker';
 import SupervisorAPI from '../src/supervisor-api';
 
 const mockedOptions = {
-	listenPort: 48484,
+	listenPort: 12345,
 	timeout: 30000,
+	dbPath: './test/data/supervisor-api.sqlite',
 };
 
 const VALID_SECRET = 'secure_api_secret';
@@ -16,11 +19,16 @@ const ALLOWED_INTERFACES = ['lo']; // Only need loopback since this is for testi
 
 describe('SupervisorAPI authentication', () => {
 	let api: SupervisorAPI;
-	const mockedConfig = new Config({ db: new Database() });
-	// Set apiSecret that we can test with
-	mockedConfig.set({ apiSecret: VALID_SECRET });
 
-	before(() => {
+	before(async () => {
+		const db = new Database({
+			databasePath: mockedOptions.dbPath,
+		});
+		await db.init();
+		const mockedConfig = new Config({ db });
+		await mockedConfig.init();
+		// Set apiSecret that we can test with
+		await mockedConfig.set({ apiSecret: VALID_SECRET });
 		api = new SupervisorAPI({
 			config: mockedConfig,
 			eventTracker: new EventTracker(),
@@ -34,9 +42,13 @@ describe('SupervisorAPI authentication', () => {
 		);
 	});
 
-	after(done => {
+	after(async () => {
 		api.stop();
-		done();
+		try {
+			await fs.unlink(mockedOptions.dbPath);
+		} catch (e) {
+			/* noop */
+		}
 	});
 
 	it('finds no apiKey and rejects', async () => {
@@ -56,14 +68,47 @@ describe('SupervisorAPI authentication', () => {
 		expect(response.statusCode).to.equal(200);
 	});
 
+	it('finds apiKey from Authorization header (Bearer scheme)', async () => {
+		const response = await postAsync(`/v1/blink`, {
+			Authorization: `Bearer ${VALID_SECRET}`,
+		});
+		expect(response.statusCode).to.equal(200);
+	});
+
+	it('finds apiKey from Authorization header (case insensitive)', async () => {
+		const randomCases = [
+			'Bearer',
+			'bearer',
+			'BEARER',
+			'BeAReR',
+			'ApiKey',
+			'apikey',
+			'APIKEY',
+			'ApIKeY',
+		];
+		for (const scheme of randomCases) {
+			const response = await postAsync(`/v1/blink`, {
+				Authorization: `${scheme} ${VALID_SECRET}`,
+			});
+			expect(response.statusCode).to.equal(200);
+		}
+	});
+
 	it('rejects invalid apiKey from query', async () => {
 		const response = await postAsync(`/v1/blink?apikey=${INVALID_SECRET}`);
 		expect(response.statusCode).to.equal(401);
 	});
 
-	it('rejects apiKey from Authorization header (ApiKey scheme)', async () => {
+	it('rejects invalid apiKey from Authorization header (ApiKey scheme)', async () => {
 		const response = await postAsync(`/v1/blink`, {
 			Authorization: `ApiKey ${INVALID_SECRET}`,
+		});
+		expect(response.statusCode).to.equal(401);
+	});
+
+	it('rejects invalid apiKey from Authorization header (Bearer scheme)', async () => {
+		const response = await postAsync(`/v1/blink`, {
+			Authorization: `Bearer ${INVALID_SECRET}`,
 		});
 		expect(response.statusCode).to.equal(401);
 	});
