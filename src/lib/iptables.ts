0720d99b63f82e769f88c9222e93bc2861f77ef3
Cameron Diver <cameron@balena.io>
2020-05-05 11:19:25 +0100

    Remove legacy fallback to DROP rule in iptables
    This has not been necessary for a long time, and wwe can now remove it.
    
    Change-type: patch
    Signed-off-by: Cameron Diver <cameron@balena.io>

--- src/lib/iptables.ts
+++ src/lib/iptables.ts
@@ -42,25 +42,19 @@ export function rejectOnAllInterfacesExcept(
 ): Bluebird<void> {
 	// We delete each rule and create it again to ensure ordering (all ACCEPTs before the REJECT/DROP).
 	// This is especially important after a supervisor update.
-	return (
-		Bluebird.each(allowedInterfaces, iface =>
-			clearAndInsertIptablesRule(
-				`INPUT -p tcp --dport ${port} -i ${iface} -j ACCEPT`,
+	return Bluebird.each(allowedInterfaces, iface =>
+		clearAndInsertIptablesRule(
+			`INPUT -p tcp --dport ${port} -i ${iface} -j ACCEPT`,
+		),
+	)
+		.then(() =>
+			clearAndAppendIptablesRule(
+				`OUTPUT -p tcp --sport ${port} -m state --state ESTABLISHED -j ACCEPT`,
 			),
 		)
-			.then(() =>
-				clearAndAppendIptablesRule(
-					`OUTPUT -p tcp --sport ${port} -m state --state ESTABLISHED -j ACCEPT`,
-				),
-			)
-			.then(() =>
-				clearAndAppendIptablesRule(`INPUT -p tcp --dport ${port} -j REJECT`),
-			)
-			// On systems without REJECT support, fall back to DROP
-			.catch(() =>
-				clearAndAppendIptablesRule(`INPUT -p tcp --dport ${port} -j DROP`),
-			)
-	);
+		.then(() =>
+			clearAndAppendIptablesRule(`INPUT -p tcp --dport ${port} -j REJECT`),
+		);
 }
 
 export function removeRejections(port: number): Bluebird<void> {
