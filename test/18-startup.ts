f6aa4dd015e8bd07c795f6c1e70adf99a6ffec94
Miguel Casqueira <mig@xgho.st>
2020-05-10 21:03:16 -0400

    Fixed stubs for test suite
    Closes: #1280
    Change-type: patch
    Signed-off-by: Miguel Casqueira <miguel@balena.io>

--- test/18-startup.ts
+++ test/18-startup.ts
@@ -1,10 +1,52 @@
+import { SinonStub, stub } from 'sinon';
+
+import APIBinder from '../src/api-binder';
+import { ApplicationManager } from '../src/application-manager';
+import DeviceState from '../src/device-state';
+import * as constants from '../src/lib/constants';
+import { DockerUtils as Docker } from '../src/lib/docker-utils';
 import { Supervisor } from '../src/supervisor';
 import { expect } from './lib/chai-config';
 
 describe('Startup', () => {
-	it('should startup correctly', function() {
+	let initClientStub: SinonStub;
+	let startStub: SinonStub;
+	let vpnStatusPathStub: SinonStub;
+	let appManagerStub: SinonStub;
+	let deviceStateStub: SinonStub;
+	let dockerStub: SinonStub;
+
+	before(() => {
+		initClientStub = stub(APIBinder.prototype as any, 'initClient').returns(
+			Promise.resolve(),
+		);
+		startStub = stub(APIBinder.prototype as any, 'start').returns(
+			Promise.resolve(),
+		);
+		appManagerStub = stub(ApplicationManager.prototype, 'init').returns(
+			Promise.resolve(),
+		);
+		vpnStatusPathStub = stub(constants, 'vpnStatusPath').returns('');
+		deviceStateStub = stub(DeviceState.prototype as any, 'applyTarget').returns(
+			Promise.resolve(),
+		);
+		dockerStub = stub(Docker.prototype, 'listContainers').returns(
+			Promise.resolve([]),
+		);
+	});
+
+	after(() => {
+		initClientStub.restore();
+		startStub.restore();
+		appManagerStub.restore();
+		vpnStatusPathStub.restore();
+		deviceStateStub.restore();
+		dockerStub.restore();
+	});
+
+	it('should startup correctly', async () => {
 		const supervisor = new Supervisor();
-		expect(supervisor.init()).to.not.throw;
+		expect(await supervisor.init()).to.not.throw;
 		// Cast as any to access private properties
 		const anySupervisor = supervisor as any;
 		expect(anySupervisor.db).to.not.be.null;
