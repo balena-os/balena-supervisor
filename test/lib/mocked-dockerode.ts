6ef3bd2362ebd185053a351490f6561ec10b84d8
Rich Bayliss <rich@balena.io>
2020-07-22 10:53:50 +0100

    docker-utils: Test network gateway determination logic
    Change-type: patch
    Signed-off-by: Rich Bayliss <rich@balena.io>

---
+++ test/lib/mocked-dockerode.ts
@@ -0,0 +1,56 @@
+import * as dockerode from 'dockerode';
+
+export interface TestData {
+	networks: Dictionary<any>;
+	images: Dictionary<any>;
+}
+
+function createMockedDockerode(data: TestData) {
+	const mockedDockerode = dockerode.prototype;
+	mockedDockerode.getNetwork = (id: string) => {
+		return {
+			inspect: async () => {
+				return data.networks[id];
+			},
+		} as dockerode.Network;
+	};
+
+	mockedDockerode.getImage = (name: string) => {
+		return {
+			inspect: async () => {
+				return data.images[name];
+			},
+		} as dockerode.Image;
+	};
+
+	return mockedDockerode;
+}
+
+export async function testWithData(
+	data: Partial<TestData>,
+	test: () => Promise<any>,
+) {
+	const mockedData: TestData = {
+		...{
+			networks: {},
+			images: {},
+			containers: {},
+		},
+		...data,
+	};
+
+	// grab the original prototype...
+	const basePrototype = dockerode.prototype;
+
+	// @ts-expect-error setting a RO property
+	dockerode.prototype = createMockedDockerode(mockedData);
+
+	try {
+		// run the test...
+		await test();
+	} finally {
+		// reset the original prototype...
+		// @ts-expect-error setting a RO property
+		dockerode.prototype = basePrototype;
+	}
+}
