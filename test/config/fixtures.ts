b068c209b0cd8cff4c2fb5218bd1d4c2116451e6
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2022-03-16 18:43:06 -0400

    Moved test setup into file included for all tests
    Change-type: patch
    Signed-off-by: 20k-ultra <3946250+20k-ultra@users.noreply.github.com>

--- test/config/fixtures.ts
+++ test/config/fixtures.ts
@@ -23,3 +23,40 @@ export const mochaGlobalSetup = function () {
 	chai.use(chaiLike);
 	chai.use(chaiThings);
 };
+
+process.env.ROOT_MOUNTPOINT = './test/data';
+process.env.BOOT_MOUNTPOINT = '/mnt/boot';
+process.env.CONFIG_JSON_PATH = '/config.json';
+process.env.CONFIG_MOUNT_POINT = './test/data/config.json';
+process.env.DATABASE_PATH = './test/data/database.sqlite';
+process.env.DATABASE_PATH_2 = './test/data/database2.sqlite';
+process.env.DATABASE_PATH_3 = './test/data/database3.sqlite';
+process.env.LED_FILE = './test/data/led_file';
+
+import * as fs from 'fs';
+
+// Make sure they are no database files left over from
+// previous runs
+try {
+	fs.unlinkSync(process.env.DATABASE_PATH);
+} catch {
+	/* noop */
+}
+try {
+	fs.unlinkSync(process.env.DATABASE_PATH_2);
+} catch {
+	/* noop */
+}
+try {
+	fs.unlinkSync(process.env.DATABASE_PATH_3);
+} catch {
+	/* noop */
+}
+fs.writeFileSync(
+	'./test/data/config.json',
+	fs.readFileSync('./test/data/testconfig.json'),
+);
+
+import '../lib/mocked-dbus';
+import '../lib/mocked-dockerode';
+import '../lib/mocked-iptables';
