7e1cafa866d7ce04d983a990aa219dbd23de26c7
Christina Ying Wang <christina@balena.io>
2024-11-08 16:49:17 -0800

    Firewall: allow DNS requests from custom Docker bridge networks
    We only allow DNS requests through `balena0` interface, but this
    is the default Docker bridge which is used for containers that
    don't have a custom bridge. However, the Supervisor creates a
    custom bridge for all containers unless another network mode is
    specified.
    
    Change-type: patch
    Signed-off-by: Christina Ying Wang <christina@balena.io>

--- src/lib/firewall.ts
+++ src/lib/firewall.ts
@@ -56,10 +56,17 @@ const standardServices: iptables.Rule[] = [
 		target: 'ACCEPT',
 	},
 	{
-		comment: 'DNS',
+		comment: 'DNS from balena0',
 		action: iptables.RuleAction.Append,
 		proto: 'udp',
 		matches: ['--dport 53', '-i balena0'],
+		target: 'ACCEPT',
+	},
+	{
+		comment: 'DNS from custom Engine networks',
+		action: iptables.RuleAction.Append,
+		proto: 'udp',
+		matches: ['--dport 53', '-i br+'],
 		target: 'ACCEPT',
 	},
 ];
