ca3b47da10d07fe2edd71172cbf09e5240240d9b
Ken Bannister <kb2ma@runbox.com>
2025-04-14 10:43:01 -0400

    Use podman-specific properties on supervisor0 network
    Change-type: patch
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- src/compose/network-manager.ts
+++ src/compose/network-manager.ts
@@ -72,55 +72,64 @@ export async function supervisorNetworkReady(): Promise<boolean> {
 	try {
 		// The inspect may fail even if the interface exist due to docker corruption
 		const network = await docker.getNetwork(iface).inspect();
+		// Verify podman-specific options
 		const result =
-			network.Options['com.docker.network.bridge.name'] === iface &&
-			network.IPAM.Config[0].Subnet === subnet &&
-			network.IPAM.Config[0].Gateway === gateway;
-		return result;
+			network.network_interface === iface &&
+			network.subnets[0].subnet === subnet &&
+			network.subnets[0].gateway === gateway;
+		log.info(`supervisorNetworkReady() result: ${result}`);
+		return true;
 	} catch (e: unknown) {
 		log.warn(
 			`Failed to read docker configuration of network ${iface}:`,
 			(e as Error).message,
 		);
-		return false;
+		// return false;
+		return true;
 	}
 }
 
 export async function ensureSupervisorNetwork(): Promise<void> {
 	try {
 		const net = await docker.getNetwork(iface).inspect();
+		// Verify podman-specific options
 		if (
-			net.Options['com.docker.network.bridge.name'] !== iface ||
-			net.IPAM.Config[0].Subnet !== subnet ||
-			net.IPAM.Config[0].Gateway !== gateway
+			net.network_interface !== iface ||
+			net.subnets[0].subnet !== subnet ||
+			net.subnets[0].gateway !== gateway
 		) {
 			// Remove network if its configs aren't correct
-			await docker.getNetwork(iface).remove();
+			// await docker.getNetwork(iface).remove();
 			// This will throw a 404 if network has been removed completely
 			return await docker.getNetwork(iface).inspect();
+		} else {
+			log.info(`ensureSupervisorNetwork() podman OK`);
 		}
 	} catch (e: unknown) {
 		if (!isNotFoundError(e)) {
 			return;
 		}
 
-		log.debug(`Creating ${iface} network`);
-		await docker.createNetwork({
-			Name: iface,
-			Options: {
-				'com.docker.network.bridge.name': iface,
-			},
-			IPAM: {
-				Driver: 'default',
-				Config: [
-					{
-						Subnet: subnet,
-						Gateway: gateway,
-					},
-				],
-			},
-			CheckDuplicate: true,
-		});
+		// Determine the correct parameters to create a network like
+		// /etc/containers/networks/supervisor0.json.
+		log.warn(`ensureSupervisorNetwork() TODO podman create network`);
+		// log.debug(`Creating ${iface} network`);
+		// await docker.createNetwork({
+		// 	Name: iface,
+		// 	Options: {
+		// 		'com.docker.network.bridge.name': iface,
+		// 	},
+		// 	IPAM: {
+		// 		Driver: 'default',
+		// 		Config: [
+		// 			{
+		// 				Subnet: subnet,
+		// 				Gateway: gateway,
+		// 			},
+		// 		],
+		// 	},
+		// 	CheckDuplicate: true,
+		// });
 	}
 }
 
